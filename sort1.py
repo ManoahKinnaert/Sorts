c636338d44b3c59a572a4fe0a4bf4f8316f2fc68
Manoah Kinnaert <manoah.k@icloud.com>
2026-03-06 15:47:28 +0100

    Better charts

--- sort1.py
+++ sort1.py
@@ -25,7 +25,7 @@ def sort(my_list: list, counter: Counter):
             exchange(my_list, j, j - 1, counter) 
             j -= 1
 
-def plot_data(average_case: list):
+def plot_data(average_case: list, worst_case: list=None, best_case: list=None):
     fig, ax = plt.subplots(1, 2)
     # plotting the compares
     ax[0].set_title("Compares")
@@ -34,6 +34,13 @@ def plot_data(average_case: list):
     ax[0].plot([n[0] for n in average_case], [n[1] for n in average_case], color='orange', label="Measured average")
     ax[0].plot([n[0] for n in average_case], [(n[0] ** 2) / 4 for n in average_case], color='orange', linestyle="dotted", lw=5, label="Expected average")
 
+    
+    ax[0].plot([n[0] for n in worst_case], [n[1] for n in worst_case], color='red', label="Measured worst")
+    ax[0].plot([n[0] for n in worst_case], [(n[0] ** 2) / 2 for n in worst_case], color='red', linestyle="dotted", lw=5, label="Expected worst")
+
+    ax[0].plot([n[0] for n in best_case], [n[1] for n in best_case], color='lime', label="Measured best")
+    ax[0].plot([n[0] for n in best_case], [n[0] - 1 for n in best_case], color='lime', linestyle="dotted", lw=5, label="Expected best")
+    
     ax[0].legend()
 
     # plotting the exchanges
@@ -43,15 +50,24 @@ def plot_data(average_case: list):
     ax[1].plot([n[0] for n in average_case], [n[2] for n in average_case], color='orange', label="Measured average")
     ax[1].plot([n[0] for n in average_case], [(n[0] ** 2) / 4 for n in average_case], color='orange', linestyle="dotted", lw=5, label="Expected average")
     
+    
+    ax[1].plot([n[0] for n in worst_case], [n[2] for n in worst_case], color='red', label="Measured worst")
+    ax[1].plot([n[0] for n in worst_case], [(n[0] ** 2) / 2 for n in worst_case], color='red', linestyle="dotted", lw=5, label="Expected worst")
+    
+    ax[1].plot([n[0] for n in best_case], [n[2] for n in best_case], color='lime', label="Measured best")
+    ax[1].plot([n[0] for n in best_case], [0 for _ in best_case], color='lime', linestyle="dotted", lw=5, label="Expected best")
+    
     ax[1].legend()
 
     plt.show()
 
 if __name__ == "__main__":
     counter = Counter()
+    results_average_case = []
+    results_worst_case = []
+    results_best_case = []
 
     # average case tests
-    results_average_case = []
     for length in range(10, 1000, 10):
         sampl = list(np.random.uniform(low=0, high=length, size=(length,)))
         print("Before: ", sampl)
@@ -59,5 +75,23 @@ if __name__ == "__main__":
         print("After: ", sampl)
         results_average_case.append((length, counter.compares, counter.exchanges))
         counter.reset()
-
-    plot_data(results_average_case)
+    
+    # worst case tests
+    for length in range(10, 1000, 10):
+        sampl = list(np.sort(np.random.uniform(0, length, length))[::-1])
+        print("Before: ", sampl)
+        sort(sampl, counter)
+        print("After: ", sampl)
+        results_worst_case.append((length, counter.compares, counter.exchanges))
+        counter.reset()
+    
+    # best case tests
+    for length in range(10, 1000, 10):
+        sampl = list(np.sort(np.random.uniform(0, length, length)))
+        print("Before: ", sampl)
+        sort(sampl, counter)
+        print("After: ", sampl)
+        results_best_case.append((length, counter.compares, counter.exchanges))
+        counter.reset()
+    
+    plot_data(results_average_case, results_worst_case, results_best_case)
