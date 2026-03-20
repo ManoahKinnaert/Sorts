685b9cf7a4b2891312bfb4e51b789dab04653635
Manoah Kinnaert <manoah.k@icloud.com>
2026-03-20 14:51:39 +0100

    Finished basic plotting, log scaled axis and stuff

--- code/sort4.py
+++ code/sort4.py
@@ -25,6 +25,7 @@ def partition_hoare(a: list, lo: int, hi: int, counter: Counter):
     return j
 
 # Lomuto partitioning
+# Not used anywhere for now
 def partition_lomuto(a: list, lo: int, hi: int, counter: Counter):
     i = lo - 1
     v = a[hi]
@@ -37,9 +38,9 @@ def partition_lomuto(a: list, lo: int, hi: int, counter: Counter):
     exchange(a, i + 1, hi, counter)
     return i + 1
 
-def sort(a: list, lo: int, hi: int, counter: Counter, lomuto: bool=False):
+def sort(a: list, lo: int, hi: int, counter: Counter):
     if (hi <= lo): return 
-    j = partition_lomuto(a, lo, hi, counter) if lomuto else partition_hoare(a, lo, hi, counter)
+    j =  partition_hoare(a, lo, hi, counter)
     sort(a, lo, j - 1, counter)
     sort(a, j + 1, hi, counter)
 
@@ -52,21 +53,25 @@ def plot_findings(case, compares, exchanges, label, color):
 
 """
 Plot the findings for the average case.
+TODO: Include theoretical curve
 """
 def plot_average_case(average_case: list, compares, exchanges):
     plot_findings(average_case, compares, exchanges, "Measured Average", "orange")
 
 """
 Plot the findings for the best case.
+TODO: Include theoretical curve
 """
 def plot_best_case(best_case: list, compares, exchanges):
     plot_findings(best_case, compares, exchanges, "Measured Best", "green")
 
 """
 Plot the findings for the worst case.
+TODO: Include theoretical curve
 """
 def plot_worst_case(worst_case: list, compares, exchanges):
     plot_findings(worst_case, compares, exchanges, "Measured Worst", "red")
+    #compares.plot([n[0] for n in worst_case], [(n[0] ** 2) / 2 for n in worst_case], color="red", label="Expected Worst", linestyle="dashdot")
 
 """
 Plot our findings.
@@ -78,10 +83,16 @@ def plot_data(average_case: list=None, worst_case: list=None, best_case: list=No
     compares.set_title("Compares")
     compares.set_xlabel("N")
     compares.set_ylabel("Count")
+    # set axis scaling for compares
+    compares.set_yscale("log")
+    compares.set_xscale("log")
     # exchanges
     exchanges.set_title("Exchanges")
     exchanges.set_xlabel("N")
     exchanges.set_ylabel("Count")
+    # set axis scaling for exchanges
+    exchanges.set_yscale("log")
+    exchanges.set_xscale("log")
     # plot the date
     if best_case is not None: plot_best_case(best_case, compares, exchanges)
     if average_case is not None: plot_average_case(average_case, compares, exchanges)
@@ -123,17 +134,27 @@ def test_worst_case(test_print: bool, length: int, results: list, counter: Count
 """
 Generates a best case starting list for Quicksort.
 """
-def best_case_list(start: int, end):
-    if start > end:
-        return []
-    mid = (start + end) // 2
-    return [mid] + best_case_list(start, mid - 1) + best_case_list(mid + 1, end)
+def best_case_list(n: int):
+    result = []
+
+    def build(values):
+        if not values:
+            return
+        mid = len(values) // 2
+        # put median first
+        result.append(values[mid])
+        # recursively process left and right
+        build(values[:mid])
+        build(values[mid+1:])
+
+    build(list(range(n)))
+    return result
 
 """
 Best case for Quicksort is an array where the pivot element is exactly in the middle (of the sorted array)
 """
 def test_best_case(test_print: bool, length: int, results: list, counter: Counter):
-    sampl = best_case_list(0, length - 1)
+    sampl = best_case_list(length)
     if test_print: print("Before: ", sampl)
     sort(sampl, lo=0, hi=len(sampl) - 1, counter=counter)
     results.append((length, counter.compares, counter.exchanges))
